8146b40032b40cc4342814f16b9e02606040d0fc
BBernard <b.brocard@codeur.online>
2022-07-11 16:08:26 +0200

    Fusion section 3

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,14 +2,18 @@
 //     alert("Uniquement sur PC");
 // }
 
-
 window.setTimeout(() => {
-
     //<section id=loader> desactiver
     document.getElementById('loader').classList.add("displayNone")
-
     //<section id="acceuil" class="displayNone"> activer
     document.getElementById('accueil').classList.remove("displayNone")
 
+}, 1000);
 
-}, 2000);
+document.getElementById('jouer').addEventListener('click', function(){
+        
+    console.log("nouvelle partie");
+    document.getElementById('accueil').classList.add("displayNone")
+    document.getElementById('jeu').classList.remove("displayNone")
+})
+ 
